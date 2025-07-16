b54dce48395bcdc122aedac763784bdc3cf1ad96
akkorek07 <akkorek47@gmail.com>
2025-07-16 09:44:02 +0200

    Delete app directory

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routers import company_routes
-import models
-from database import engine
-
-app = FastAPI()
-models.Base.metadata.create_all(bing=engine)
-
-app.include_router(company_routes.router)
